580bd92b7a5cd7cfb9240641a3ed41adb0ef941f
alevento <alessandro.manneschi@gmail.com>
2015-11-10 17:43:44 +0100

    change config mail

--- src/config/composer-security-check.php
+++ src/config/composer-security-check.php
@@ -4,6 +4,6 @@ return array(
     'mailSubjetcAlarm' => env('SECURITY_CHECK_SUBJECT_ALARM', '[composer-security-check]: Alarm - vulnerabilities detected.'),
     'mailFrom' => env('SECURITY_CHECK_MESSAGE_FROM', 'info@example.com'),
     'mailFromName' => env('SECURITY_CHECK_MESSAGE_FROM_NAME', 'Info Example'),
-    'mailViewName' => env('SECURITY_CHECK_MAIL_VIEW_NAME', 'security-check.mail'),
+    'mailViewName' => env('SECURITY_CHECK_MAIL_VIEW_NAME', 'composer-security-check::mail'),
     'logFilePath' => env('SECURITY_CHECK_LOG_FILE_PATH', storage_path().'/composersecurityCheck.log')
  );
