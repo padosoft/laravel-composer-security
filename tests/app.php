3ffcfa930eb25c642bd765da7f90c3fce899a8c9
alevento <alessandro.manneschi@gmail.com>
2015-12-23 16:22:20 +0100

    Commit 32

--- tests/app.php
+++ tests/app.php
@@ -144,10 +144,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
-
-        Padosoft\Composer\ComposerSecurityCheckServiceProvider::class,
 
     ],
 
@@ -196,7 +192,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Debugbar'  => Barryvdh\Debugbar\Facade::class,
+
     ],
 
 ];
